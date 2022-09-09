274c23fd88a5b0899acb57346a5fb543cd7cbb94
Harsh <harshky24@gmail.com>
2022-09-09 19:28:58 +0530

    initial Commit

--- Code/lib/frontend/mobile/mobile_login_signup_page.dart
+++ Code/lib/frontend/mobile/mobile_login_signup_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mi_hackathon/backend/globalELement.dart';
 
 class Mobile_LogIn_SignUp_Page extends StatefulWidget {
   const Mobile_LogIn_SignUp_Page({Key? key}) : super(key: key);
@@ -11,11 +12,39 @@ class Mobile_LogIn_SignUp_Page extends StatefulWidget {
 class _Mobile_LogIn_SignUp_PageState extends State<Mobile_LogIn_SignUp_Page> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      child: Text('Mobile'),
-      width: 100,
-      height: 100,
-    );
+    return Stack(children: [
+      Positioned(
+        top: 10,
+        right: 10,
+        left: 10,
+        height: getHeight(context, 0.2),
+        child: Container(
+          color: subBackgroundColor,
+        ),
+      ),
+      Column(
+        children: [
+          Center(
+              child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Image.asset(
+              'assets/images/xiaomi_logo.png',
+              height: getHeight(context, 0.1),
+            ),
+          )),
+          SizedBox(
+            height: 20,
+          ),
+          Positioned(
+              top: 0,
+              right: 20,
+              left: 20,
+              child: Container(
+                color: Colors.red,
+                height: 10,
+              ))
+        ],
+      ),
+    ]);
   }
 }

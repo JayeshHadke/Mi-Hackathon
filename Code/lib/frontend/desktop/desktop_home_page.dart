c75e4df7c16862e4fa36f1645c1f6349653f3b29
Jayesh Hadke <jayeshhadkejrh@gmail.com>
2022-09-10 16:51:39 +0530

    home page for desktop

--- Code/lib/frontend/desktop/desktop_home_page.dart
+++ Code/lib/frontend/desktop/desktop_home_page.dart
@@ -12,6 +12,14 @@ class Desktop_Home_Page extends StatefulWidget {
   State<Desktop_Home_Page> createState() => _Desktop_Home_PageState();
 }
 
+enum drawerItems {
+  Home,
+  Orders,
+  Account,
+}
+
+var subPage = drawerItems.Home;
+
 class _Desktop_Home_PageState extends State<Desktop_Home_Page> {
   String name = currentUser!.name;
   String email = currentUser!.email;
@@ -26,29 +34,98 @@ class _Desktop_Home_PageState extends State<Desktop_Home_Page> {
         backgroundColor: subBackgroundColor,
         elevation: 5,
         title: Container(
-            child: Row(
-          children: [
-            Image.asset(
-              'assets/images/xiaomi_logo_nolabel.png',
-              height: getHeight(context, 0.06),
-            ),
-            SizedBox(
-              width: getWidth(context, 0.01),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                mainText(context: context, str: '$name - $posId'),
-                subText(context: context, str: storeName),
-              ],
-            )
-          ],
-        )),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/images/xiaomi_logo_nolabel.png',
+                height: getHeight(context, 0.06),
+              ),
+              SizedBox(
+                width: getWidth(context, 0.01),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  mainText(context: context, str: '$name - $posId'),
+                  subText(context: context, str: storeName),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
       drawer: Drawer(
-          child: ListView(
-        children: [DrawerHeader(child: Text('data'))],
-      )),
+        child: ListView(
+          children: [
+            DrawerHeader(
+              margin: EdgeInsets.only(
+                top: getHeight(
+                  context,
+                  0.04,
+                ),
+              ),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage('assets/images/xiaomi_logo.png'),
+                ),
+              ),
+              child: Container(),
+            ),
+            Divider(
+              height: getHeight(context, 0.08),
+              color: subBackgroundColor,
+              thickness: 2,
+              indent: getWidth(context, 0.015),
+              endIndent: getWidth(context, 0.015),
+            ),
+            ListTile(
+              onTap: () {
+                setState(() {
+                  subPage = drawerItems.Home;
+                });
+              },
+              selectedTileColor: Colors.grey.shade200,
+              selectedColor: mainBackgroundColor,
+              selected: subPage == drawerItems.Home,
+              leading: const Icon(Icons.home_rounded),
+              title: mainText(context: context, str: 'Home'),
+            ),
+            ListTile(
+              onTap: () {
+                setState(() {
+                  subPage = drawerItems.Orders;
+                });
+              },
+              selectedTileColor: Colors.grey.shade200,
+              selectedColor: mainBackgroundColor,
+              selected: subPage == drawerItems.Orders,
+              leading: const Icon(Icons.shopping_cart_rounded),
+              title: mainText(context: context, str: 'Orders'),
+            ),
+            ListTile(
+              onTap: () {
+                setState(() {
+                  subPage == drawerItems.Account;
+                });
+              },
+              selected: subPage == drawerItems.Account,
+              selectedTileColor: Colors.grey.shade200,
+              selectedColor: mainBackgroundColor,
+              leading: const Icon(Icons.account_circle_rounded),
+              title: mainText(context: context, str: 'Account'),
+            ),
+            ListTile(
+              onTap: () {},
+              selectedTileColor: Colors.grey.shade200,
+              selectedColor: mainBackgroundColor,
+              leading: const Icon(Icons.exit_to_app_rounded),
+              title: mainText(context: context, str: 'Exit'),
+            ),
+          ],
+        ),
+      ),
       // body: Stack(
       //   children: [
       //     Positioned(
--- Code/lib/main.dart
+++ Code/lib/main.dart
@@ -20,7 +20,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'BalooBhai2'),
+      theme: ThemeData(
+        fontFamily: 'BalooBhai2',
+        dividerColor: Colors.transparent,
+      ),
       debugShowCheckedModeBanner: false,
       home: const SafeArea(
         child: LogIn_SignUp_Page(),
